7a4923e1d632f39ef39ff4f897cc9b1562b00e48
rjsteinert <rj@rjsteinert.com>
2013-02-28 09:52:53 -0500

    Working example of loading a remote video.

---
+++ gen/com/example/HTML5VideoInAndroidWebViewExample/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.HTML5VideoInAndroidWebViewExample;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
